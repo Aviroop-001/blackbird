add1aeea3cc940861557bad0ed3e1d4a4595fa0a
Aviroop-001 <banerjeeaviroop01@gmail.com>
2023-01-16 12:00:10 +0530

    first commit

---
+++ command.sh
@@ -0,0 +1,7 @@
+npx create-react-app blackbird
+cd .\blackbird\
+git init
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Aviroop-001/blackbird.git
+git push -u origin main
